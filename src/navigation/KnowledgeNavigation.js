8a3830dd03f37fc78a2ba0036c916e3f0113f759
harinath <harinath@docintosh.com>
2023-04-19 11:50:11 +0530

    data

--- src/navigation/KnowledgeNavigation.js
+++ src/navigation/KnowledgeNavigation.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import HandleBack from './HandleBack';
 import { showHeaderItem ,showHeaderItemBackless} from './ReuseLogics';
-import SavedPost from '../screens/Settings/SavedPost/SavedPost';
 import BlockList from '../screens/Settings/BlockList/BlockList';
 import PdfViewer from '../screens/Knowledge/PdfViewer';
 import KnowledgeScreen from '../screens/Knowledge/KnowledgeScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartModules from '../screens/Knowledge/StartModules';
-import getStartedModule from '../screens/Knowledge/GetStarted/getStartedModule';
 import AssesmentTest from '../screens/Knowledge/GetStarted/AssesmentTest';
+import GetStartedModule from '../screens/Knowledge/GetStarted/GetStartedModule';
 
 
 const KnowledgeNavigation = () => {
@@ -21,7 +20,7 @@ const KnowledgeNavigation = () => {
         <Stack.Screen name="BlockList" component={BlockList} options={showHeaderItem} />
         <Stack.Screen name="PdfViewer" component={PdfViewer} options={showHeaderItem} />
         <Stack.Screen name="StartModules" component={StartModules} options={showHeaderItem} />
-        <Stack.Screen name="getStartedModule" component={getStartedModule} options={showHeaderItem} />
+        <Stack.Screen name="GetStartedModule" component={GetStartedModule} options={showHeaderItem} />
         <Stack.Screen name="AssesmentTest" component={AssesmentTest} options={showHeaderItem} />
     </Stack.Navigator>
     </>
